400029e95b84c94f058518fa8bf725c8d3ac9ec4
Muhammad Usman <ch.usman564@gmail.com>
2022-04-06 19:05:56 +0500

    Fix navigation animation

--- app/app.js
+++ app/app.js
@@ -16,19 +16,28 @@ console.log(heroContent);
 hamburger.addEventListener('click', function (e) {
   const icon = e.target;
   if (!(icon.classList.contains('hamburger__icon'))) return;
-
   icon.classList.toggle('icon-cross');
-  header.classList.toggle('header-white');
+  overlay.classList.add('display-block');
+  overlay.classList.remove('overlay-show');
+  header.classList.add('header-white');
   nav.classList.toggle('show-navigation');
   nav.classList.toggle('hidden');
-  overlay.classList.toggle('overlay-show');
   navLink.forEach(link => {
     link.classList.toggle('color-black');
   })
+  if (header.classList.contains("header-white")) {
+    nav.classList.remove('position-absolute');
+  }
+  logo.classList.add('logo-hide');
   setTimeout(function () {
-    logo.classList.toggle('logo-hide');
-    nav.classList.toggle('position-absolute');
-  }, 400);
+    overlay.classList.add('overlay-show');
+    if (!(icon.classList.contains('icon-cross'))) {
+      overlay.classList.remove('display-block');
+      header.classList.remove('header-white');
+      nav.classList.add('position-absolute');
+      logo.classList.remove('logo-hide');
+    }
+  }, 600);
 
 });
 
